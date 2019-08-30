65d781f3ad292334262efd8898bdf14178d03d09
Thomas Parisot <oncletom@users.noreply.github.com>
2019-08-30 12:47:39 +0200

    fix: loading button

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-import {axisBottom, axisLeft} from 'https://unpkg.com/d3-axis@1?module'
-import {select} from 'https://unpkg.com/d3-selection@1?module'
-import {scaleLinear} from 'https://unpkg.com/d3-scale@2?module'
-import {json, csv} from 'https://unpkg.com/d3-fetch@1?module'
+import {axisBottom, axisLeft} from 'https://unpkg.com/d3-axis@^1?module'
+import {select} from 'https://unpkg.com/d3-selection@^1?module'
+import {scaleLinear} from 'https://unpkg.com/d3-scale@^2?module'
+import {json, csv} from 'https://unpkg.com/d3-fetch@^1?module'
 
+const containerElement = document.querySelector('#graph').parentElement
 const HEIGHT = 250
-const WIDTH = document.querySelector('#graph').parentElement.clientWidth
+const WIDTH = containerElement.clientWidth
 const GUTTER = 30
 const MARGIN = 10
 
-const graph = select('#graph')
 const xScale = scaleLinear().domain([0, 110]).range([0, WIDTH-GUTTER-MARGIN])
 const xAxis = axisBottom(xScale).ticks(10)
 const yScale = scaleLinear().domain([1023, 90]).range([0, HEIGHT-GUTTER-MARGIN])
 const yAxis = axisLeft(yScale).ticks(6)
 
-graph
+select('#graph').remove()
+const graph = select(containerElement)
+  .append('svg')
+  .attr('id', 'graph')
   .attr('viewBox', [0, 0, WIDTH, HEIGHT])
   .attr('height', '250')
   .attr('width', '100%')
